8da680fcfecef775b17afc54f68b888826752c15
ea-k <121165806+ea-k@users.noreply.github.com>
2025-12-06 15:45:29 +0100

    #Issue 17 : creation d'un instance manager pour stocker et créer des instances

---
+++ src/instances/instances.py
@@ -0,0 +1,93 @@
+from dataclasses import dataclass
+from typing import List, Tuple
+import json
+
+# changer config pour rajoute le nom de l'instance agent et event
+# changer setting pour rajouter l'ui des instances agent 
+# changer fun init des position dans algo 
+
+#chercher ou modifier pour event 
+
+@dataclass
+class Instance:
+    name :str
+    id:int
+    
+    
+@dataclass
+class InstanceAgent(Instance):
+    
+    nb_agent:int
+    position: List[Tuple[int, int]]
+    map_name: str
+    
+@dataclass
+class InstanceEvent(Instance):
+    #TODO
+    event_type: str
+    
+    
+
+class InstanceManager:
+    instancesJSON= "src\instances\instances.json"
+    
+    def __init__(self):
+        self.agent_instances = []
+        self.event_instances = []
+        with open(self.instancesJSON, "r") as eventfile:
+            self.manager = json.load(eventfile)
+        self.readJSON()
+        
+    def readJSON(self):
+        # iterate over all type of instances
+        for i in self.manager:
+            temp : Instance
+            #iterate over all agent instances
+            if i == "agents":
+                for agent in self.manager[i]:
+                    temp = InstanceAgent(name=agent["name"],
+                                         id=agent["id"],
+                                         nb_agent=agent["nb_agent"]
+                                         ,map_name=agent["map_name"],
+                                         position=agent["position"])
+                    self.agent_instances.append(temp)
+                
+            #iterate over all event instances
+            elif i == "events":
+                for event in self.manager[i]:
+                    temp = InstanceEvent(name=event["name"],
+                                         id=event["id"],
+                                         event_type=event["event_type"])
+                    self.event_instances.append(temp)
+                    
+    
+    def getAllInstances(self):
+        return  self.event_instances + self.agent_instances
+    
+    def getAllAgentInstances(self):
+         return self.agent_instances
+    
+    def getAllEventInstances(self):
+         return  self.event_instances
+     
+    def getAgentInstancesByMap(self, map_name):
+        return [ i for i in self.agent_instances if (i.map_name == map_name)]
+    
+    
+    #TODO get agent by map, add new instance      get selected instance or config ?
+        
+    def writeJSON(self):
+        toJSON = {
+            "agents": self.agent_instances,
+            "events": self.event_instances
+        }
+        with open(self.instancesJSON, "w") as outfile:
+            json.dump(toJSON, outfile,default=vars, indent=4)
+        outfile.close()
+            
+    
+
+test = InstanceManager()
+
+#self.agent_instances.append(InstanceAgent(name="name",id=5,nb_agent=15,map_name="map_name",position=[(4,5),(6,7)]))
+        
